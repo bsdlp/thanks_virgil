521f630e60b2279ebe5cc486db9d49c3ecf44dbd
Jon Chen <bsd@voltaire.sh>
2014-02-07 03:52:37 -0500

    we will not need a db (for now)

--- manage.py
+++ manage.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python2
 
-from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager
 import app
 
-migrate = Migrate(app.app, app.db)
-
 manager = Manager(app.app)
-manager.add_command('db', MigrateCommand)
 
 if __name__ == "__main__":
     manager.run()
