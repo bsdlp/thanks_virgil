48d44b82dcbe62b390d192125dd04c0535744e43
Jon Chen <bsd@voltaire.sh>
2014-02-17 01:10:00 -0500

    update inventory_host to inventory_hostname

--- lib/thanks_virgil/ansible-playbooks/roles/csgo/templates/start.sh
+++ lib/thanks_virgil/ansible-playbooks/roles/csgo/templates/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /srv/steam/
-./srcds_run -game csgo -usercon -strictportbind -ip {{ hostvars[inventory_host]['ansible_eth0']['ipv4']['address'] }} -port 27015 +clientport 27005 +tv_port 27020 -tickrate 128 +map de_dust2 +servercfgfile server.cfg -maxplayers_override 12 +mapgroup random_classic +game_mode 1 +game_type 0
+./srcds_run -game csgo -usercon -strictportbind -ip {{ hostvars[inventory_hostname]['ansible_eth0']['ipv4']['address'] }} -port 27015 +clientport 27005 +tv_port 27020 -tickrate 128 +map de_dust2 +servercfgfile server.cfg -maxplayers_override 12 +mapgroup random_classic +game_mode 1 +game_type 0
 
