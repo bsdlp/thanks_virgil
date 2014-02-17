afc51568ee7234e6f426ff3616a951de574094eb
Jon Chen <bsd@voltaire.sh>
2014-02-17 10:30:36 -0500

    fix start script cwd

--- lib/thanks_virgil/ansible-playbooks/roles/csgo/templates/start.sh
+++ lib/thanks_virgil/ansible-playbooks/roles/csgo/templates/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd /srv/steam/
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $CWD
 ./srcds_run -game csgo -usercon -strictportbind -ip {{ hostvars[inventory_hostname]['ansible_eth0']['ipv4']['address'] }} -port 27015 +clientport 27005 +tv_port 27020 -tickrate 128 +map de_dust2 +servercfgfile server.cfg -maxplayers_override 12 +mapgroup random_classic +game_mode 1 +game_type 0
 
