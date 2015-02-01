0bc08a53694c057344451e2138d32e1f53675f8e
Jon Chen <bsd@voltaire.sh>
2015-01-31 23:45:21 -0800

    implement argparse

--- lib/thanks_virgil/run.py
+++ lib/thanks_virgil/run.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python2
 
-import os
-ssh_pubkey =osrgv[0]
+import argparse
+
+def parse():
+    parser = argparse.ArgumentParser(description='thanks_virgil')
+    parser.add_argument('--ssh-pubkey', action='store')
+    return parser
+
+if __name__ == '__main__':
+    args = parse()
